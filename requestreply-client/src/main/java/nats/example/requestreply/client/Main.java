b3184fb12332b9b0937ad3783807e860bd537341
gregwhitaker <gwhitake@gmail.com>
2018-01-25 20:49:27 -0800

    Doing work

--- requestreply-client/src/main/java/nats/example/requestreply/client/Main.java
+++ requestreply-client/src/main/java/nats/example/requestreply/client/Main.java
@@ -1,4 +1,15 @@
 package nats.example.requestreply.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) {
+
+    }
 }
